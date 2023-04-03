2c1ab4db1eb15a7488bb04feb5a347cd7fbc921b
ALexandrNiG <101365315+ALexandrNiG@users.noreply.github.com>
2023-04-04 02:48:08 +0300

    Add files via upload

---
+++ basics_2_lesson/task_4.py
@@ -0,0 +1,12 @@
+# Вариант 1
+a = 1
+i = 1
+while i <= 10:
+    a *= i
+    i += 1
+print(a)
+
+# Вариант 2
+import math
+
+print(math.factorial(10))
---
+++ basics_2_lesson/task_5.py
@@ -0,0 +1,6 @@
+a = str(123456)
+
+i = 0
+while i < 6:
+    print(a[i])
+    i += 1
